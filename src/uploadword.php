0e1e664b4649169600a00cb6e18c79749c3e78d0
AnkitVig <ankitkumarvig@gmail.com>
2020-10-06 01:24:07 -0400

    Word Error Fix

--- src/uploadword.php
+++ src/uploadword.php
@@ -1,10 +1,16 @@
 <?php
+
+/*
+ * File to load excel file in browser using Word file.
+ */
 include '../vendor/autoload.php';
 
 use PhpOffice\PhpWord\IOFactory;
 use PhpOffice\PhpWord;
 
-
+/*
+ * Validating file and extensions.
+ */
 if($_FILES["filename"]["name"] != '')
 {
     $allowed_extension = array('docx', 'doc');
@@ -12,11 +18,37 @@ if($_FILES["filename"]["name"] != '')
     $file_extension = end($file_array);
     if(in_array($file_extension, $allowed_extension))
     {
-        $reader = IOFactory::createReader('Word2007');
-        $word = $reader->load($_FILES['filename']['tmp_name']);
-        $writer = IOFactory::createWriter($word, 'Word2007');
-        $writer->save('php://output');
-        $message = readfile('php://output');
+        $phpWord = IOFactory::createReader('Word2007')->load($_FILES['filename']['tmp_name']);
+
+        $text = [];
+
+
+        /*
+         * Getting data of each section into array.
+         */
+
+        foreach ($phpWord->getSections() as $section) {
+
+            foreach ($section->getElements() as $element) {
+
+                switch (get_class($element)) {
+                    case 'PhpOffice\PhpWord\Element\Text' :
+                        $text[] = $element->getText();
+                        break;
+                    case 'PhpOffice\PhpWord\Element\TextRun':
+                        $textRunElements = $element->getElements();
+                        foreach ($textRunElements as $textRunElement) {
+                            $text[] = $textRunElement->getText();
+                        }
+                        break;
+                    case 'PhpOffice\PhpWord\Element\TextBreak':
+                        $text[] = " ";
+                        break;
+                    default:
+                        throw new Exception('Something went wrong...');
+                }
+            }
+        }
 
     }
     else
@@ -29,6 +61,12 @@ else
     $message = '<div class="alert alert-danger">Please Select File</div>';
 }
 
-echo $message;
+echo '<textarea>';
+foreach($text as $key )
+{
+    echo $key;
+}
+
+echo '</textarea>';
 
 ?>
--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
    <head>
-     <title>Load Excel Sheet in Browser using PHPSpreadsheet</title>
+     <title>Load Documents in Browser using PHPSpreadsheet and PHPWord</title>
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
    </head>
@@ -11,30 +11,37 @@
       <h3 align="center">Load Excel Sheet in Browser using PHPSpreadsheet</h3>
       <br />
       <div class="table-responsive">
-       <span id="message"></span>
-          <form method="post" id="load_excel_form" enctype="multipart/form-data">
-            <table class="table">
-              <tr>
-                <td width="25%" align="right">Select Excel File</td>
-                <td width="50%"><input type="file" id ="filename" name="filename" /></td>
-                <td width="25%"><input type="submit" name="load" class="btn btn-primary" /></td>
-              </tr>
-            </table>
-          </form>
-       <br />
-          <div id="excel_area"></div>
-      </div>
+          <!-- Excel Form Loading Data -->
+      <span id="message"></span>
+         <form method="post" id="load_excel_form" enctype="multipart/form-data">
+           <table class="table">
+             <tr>
+               <td width="25%" align="right">Select Excel File</td>
+               <td width="50%"><input type="file" id ="filename" name="filename" /></td>
+               <td width="25%"><input type="submit" name="load" class="btn btn-primary" /></td>
+             </tr>
+           </table>
+         </form>
+      <br />
+         <div id="excel_area"></div>
      </div>
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-  </body>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+ </body>
 </html>
+
+<!-- Script to upload the file  -->
 <script>
 $(document).ready(function(){
-  $('#load_excel_form').on('submit', function(event){
-    event.preventDefault();
-  
-    
-    $.ajax({
+$('#load_excel_form').on('submit', function(event){
+  event.preventDefault();
+    var ln = document.getElementById("filename").value;
+
+ /*
+  * Check for document type
+  */
+if(ln.includes("docx")||ln.includes("doc")){
+  $.ajax({
       url:"../src/uploadword.php",
       type:"POST",
       data:new FormData(this),
@@ -43,11 +50,28 @@ $(document).ready(function(){
       processData:false,
       success:function(data)
       {
-        $('#excel_area').html(data);
-        $('table').css('width','100%');
+          $('#excel_area').html(data);
+          $('table').css('width','100%');
       }
-    })
-  });
+  })
+}
+   else if(ln.includes("xls")||ln.includes("xlsx")){
+        $.ajax({
+            url:"../src/upload.php",
+            type:"POST",
+            data:new FormData(this),
+            contentType:false,
+            cache:false,
+            processData:false,
+            success:function(data)
+            {
+                $('#excel_area').html(data);
+                $('table').css('width','100%');
+            }
+        })
+    }
+
+});
 });
 </script>
 
